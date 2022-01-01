d9cf475fce505bb8dccee85e07025ab59381936f
geist-2501 <blaironbute@gmail.com>
2022-01-01 23:48:16 +0000

    Added basic sphere detection

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,26 @@
 import java.io.File
 
+fun rayColour(r: Ray): Colour {
+    if (hitSphere(Vec3(0, 0, -1), 0.5, r)) {
+        return Colour(1, 0, 0)
+    }
+
+    val unitDir = r.direction.unit()
+    val t = 0.5 * (unitDir.y + 1.0)
+    return (1.0 - t) * Colour(1, 1, 1) + t * Colour(0.5, 0.7, 1.0)
+}
+
+fun hitSphere(center: Vec3, radius: Double, r: Ray): Boolean {
+
+    val oc = r.origin - center
+    val a = Vec3.dot(r.direction, r.direction)
+    val b = 2.0 * Vec3.dot(oc, r.direction)
+    val c = Vec3.dot(oc, oc) - radius * radius
+    val discriminant = b*b - 4 * a * c
+
+    return discriminant > 0
+}
+
 fun main(args: Array<String>) {
 
     val f = File("img.ppm").bufferedWriter()
@@ -43,8 +64,3 @@ fun main(args: Array<String>) {
     println("Done!")
 }
 
-fun rayColour(r: Ray): Colour {
-    val unitDir = r.direction.unit()
-    val t = 0.5 * (unitDir.y + 1.0)
-    return (1.0 - t) * Colour(1, 1, 1) + t * Colour(0.5, 0.7, 1.0)
-}
